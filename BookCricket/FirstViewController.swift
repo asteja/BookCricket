fac19ba61a3e51abcafaf1e573807e8cb6f37729
Saiteja Alle <alle.saiteja@gmail.com>
2017-02-03 11:27:50 -0500

    first commit

---
+++ BookCricket/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  BookCricket
+//
+//  Created by Saiteja Alle on 2/3/17.
+//  Copyright © 2017 Saiteja Alle. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
